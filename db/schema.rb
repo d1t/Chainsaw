d2ba289bd6b254b8b05f90f26ad0f47a2e5ca9f0
Alejandro Babio <alejandro.e.babio@hotmail.com>
2015-11-26 09:58:49 -0300

    Brand bio & edit profile improvements
    * Fix show store image as circle 150 x 150
    * Add brand_bio field to store, maximum 140 chars
    * Set permitted_params for devise registration#edit
    * Improve edit profile form to show image, and remove

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120192454) do
+ActiveRecord::Schema.define(version: 20151126103542) do
 
   create_table "partners", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20151120192454) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.integer  "partner_id"
+    t.string   "brand_bio"
   end
 
   add_index "stores", ["partner_id"], name: "index_stores_on_partner_id"
